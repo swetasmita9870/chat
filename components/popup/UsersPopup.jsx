633f6a2a31f661f8cd0a85ee16461375f5bc9c7d
swetasmita9870 <78337359+swetasmita9870@users.noreply.github.com>
2023-07-01 23:01:58 +0530

    Add Key to usrrpop

--- components/popup/UsersPopup.jsx
+++ components/popup/UsersPopup.jsx
@@ -85,7 +85,7 @@ const UsersPopup = (props) => {
                 <div className="absolute w-full">
                     {users &&
                         Object.values(users).map((user, index) => (
-                            <div key={index}
+                            <div key={user.uid}
                                 className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer"
                                 onClick={() => handleSelect(user)}
                             >
